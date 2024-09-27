b66bd94597fd504a10212998464a7d606ff5e71f
MilenaGarciaCosta <milenagarcia.dev@gmail.com>
2024-09-26 21:01:15 -0300

    classes debugadas

--- src/routes/Musicas.jsx
+++ src/routes/Musicas.jsx
@@ -2,13 +2,13 @@ import '../css/MusicasStyle.css';
 
 const Musicas = () => {
   return (
-    <section classNameName='mainSection'>
+    <section className='mainSection'>
       <h2>Músicas</h2>
-      <main classNameName='cardMusicaContainer'>
+      <main className='cardMusicaContainer'>
 
         <div className="card">
           <div className="image_container">
-            <img classNameName='image' src="https://images.tcdn.com.br/img/img_prod/435678/lp_vinil_linkin_park_hybrid_theory_8005_1_3182aadfb04c191cd270e32399a4ac3c.jpg" alt="" />
+            <img className='image' src="https://images.tcdn.com.br/img/img_prod/435678/lp_vinil_linkin_park_hybrid_theory_8005_1_3182aadfb04c191cd270e32399a4ac3c.jpg" alt="" />
           </div>
           <div className="title">
             <span>Link Park Hybrid Theory</span>
@@ -46,7 +46,7 @@ const Musicas = () => {
         
         <div className="card">
           <div className="image_container">
-            <img classNameName='image' src="https://cdn.shopify.com/s/files/1/0105/4542/products/gorillaz-demondays_1200x.jpg?v=1524582819" alt="" />
+            <img className='image' src="https://cdn.shopify.com/s/files/1/0105/4542/products/gorillaz-demondays_1200x.jpg?v=1524582819" alt="" />
           </div>
           <div className="title">
             <span>Gorillaz Demon days</span>
@@ -84,7 +84,7 @@ const Musicas = () => {
 
         <div className="card">
           <div className="image_container">
-            <img classNameName='image' src="https://th.bing.com/th/id/OIP.h-2F4w3D5rR0DEXoDpgXiQHaHa?rs=1&pid=ImgDetMain" alt="" />
+            <img className='image' src="https://th.bing.com/th/id/OIP.h-2F4w3D5rR0DEXoDpgXiQHaHa?rs=1&pid=ImgDetMain" alt="" />
           </div>
           <div className="title">
             <span>Cartola 1976</span>
@@ -122,7 +122,7 @@ const Musicas = () => {
 
         <div className="card">
           <div className="image_container">
-            <img classNameName='image' src="https://http2.mlstatic.com/D_NQ_NP_734755-MLU69125972265_042023-O.webp" alt="" />
+            <img className='image' src="https://http2.mlstatic.com/D_NQ_NP_734755-MLU69125972265_042023-O.webp" alt="" />
           </div>
           <div className="title">
             <span>Pitty Anacronico</span>
@@ -160,7 +160,7 @@ const Musicas = () => {
 
         <div className="card">
           <div className="image_container">
-            <img classNameName='image' src="https://i.ebayimg.com/images/g/gUgAAOSwSONixyQa/s-l1600.jpg" alt="" />
+            <img className='image' src="https://i.ebayimg.com/images/g/gUgAAOSwSONixyQa/s-l1600.jpg" alt="" />
           </div>
           <div className="title">
             <span>Billie Elish don't smile at me</span>
@@ -198,7 +198,7 @@ const Musicas = () => {
 
         <div className="card">
           <div className="image_container">
-            <img classNameName='image' src="https://m.media-amazon.com/images/I/51BZVcGK8IL._AC_SX522_.jpg" alt="" />
+            <img className='image' src="https://m.media-amazon.com/images/I/51BZVcGK8IL._AC_SX522_.jpg" alt="" />
           </div>
           <div className="title">
             <span>Angra Cycles of pain</span>
@@ -235,7 +235,7 @@ const Musicas = () => {
 
         <div className="card">
           <div className="image_container">
-            <img classNameName='image' src="https://images-na.ssl-images-amazon.com/images/I/7115nyXjFqL._SL1329_.jpg" alt="" />
+            <img className='image' src="https://images-na.ssl-images-amazon.com/images/I/7115nyXjFqL._SL1329_.jpg" alt="" />
           </div>
           <div className="title">
             <span>Joji Ballads</span>
@@ -273,7 +273,7 @@ const Musicas = () => {
 
         <div className="card">
           <div className="image_container">
-            <img classNameName='image' src="https://th.bing.com/th/id/OIP.EeDAVxfzWTqbHNTbO2cVsgHaFj?rs=1&pid=ImgDetMain" alt="" />
+            <img className='image' src="https://th.bing.com/th/id/OIP.EeDAVxfzWTqbHNTbO2cVsgHaFj?rs=1&pid=ImgDetMain" alt="" />
           </div>
           <div className="title">
             <span>The neighbourhood hard to imagine</span>
@@ -310,7 +310,7 @@ const Musicas = () => {
 
         <div className="card">
           <div className="image_container">
-            <img classNameName='image' src="https://i5.walmartimages.com/seo/Evanescence-Fallen-20th-Anniversary-Edition-Deluxe-Edition-Pink-Black-Marble-Colored-Vinyl-2-LP_48b5a4ba-ce7c-4861-8f3a-1c891e32404c.540864ada3af8a65464d1e092d979b01.jpeg" alt="" />
+            <img className='image' src="https://i5.walmartimages.com/seo/Evanescence-Fallen-20th-Anniversary-Edition-Deluxe-Edition-Pink-Black-Marble-Colored-Vinyl-2-LP_48b5a4ba-ce7c-4861-8f3a-1c891e32404c.540864ada3af8a65464d1e092d979b01.jpeg" alt="" />
           </div>
           <div className="title">
             <span>Evanescence Fallen</span>
@@ -347,7 +347,7 @@ const Musicas = () => {
 
         <div className="card">
           <div className="image_container">
-            <img classNameName='image' src="https://th.bing.com/th/id/OIP.cSPWA5wUlvfqLI6eSdw9jQHaHa?rs=1&pid=ImgDetMain" alt="" />
+            <img className='image' src="https://th.bing.com/th/id/OIP.cSPWA5wUlvfqLI6eSdw9jQHaHa?rs=1&pid=ImgDetMain" alt="" />
           </div>
           <div className="title">
             <span>Fleetwood mac Rumours</span>
